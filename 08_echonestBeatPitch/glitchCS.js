ec1e85743cc46e7d2a2498efc4d0932e026c8495
Chad Smith <chadxsmith@ymail.com>
2016-04-20 22:44:30 -0400

    pushing issue w/ sketch.js returing error

---
+++ 08_echonestBeatPitch/glitchCS.js
@@ -0,0 +1,388 @@
+ $( document ).ready(function() {
+     
+    $(".source").css('background-image','url(/images/stop.png)');
+    $(".source").css('visibility','visible');
+
+
+function glitchPic(){
+     
+        if (counter === 5) {
+          
+
+        }
+        // else if ( counter > 0){
+        //     console.log("begin!")
+        //     console.log(counter)
+        // }
+       //  else if (counter === 10){
+       //       $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+       //       // console.log(counter)
+            
+             
+       //  }
+       //  else if (counter === 11){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 20){
+       //       $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+       //  }
+       //  else if (counter === 21){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 30){
+       //       $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+       //  }
+       //  else if (counter === 31){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 40){
+       //       $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+       //  }
+       //  else if (counter === 41){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 40){
+       //       $(".source").css('background-image','url(/images/artworks-000041771384-nl4e9m-t500x500.jpg)');
+       //  }
+       //  else if (counter === 41){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 50){
+       //       $(".source").css('background-image','url(/images/artworks-000041771384-nl4e9m-t500x500.jpg)');
+       //  }
+       //  else if (counter === 51){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 60){
+       //       $(".source").css('background-image','url(/images/artworks-000041771384-nl4e9m-t500x500.jpg)');
+       //  }
+       //  else if (counter === 61){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 70){
+       //       $(".source").css('background-image','url(/images/artworks-000041771384-nl4e9m-t500x500.jpg)');
+       //  }
+       //  else if (counter === 71){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 80){
+       //       $(".source").css('background-image','url(/images/chris.jpg)');
+       //  }
+       //  else if (counter === 81){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 90){
+       //       $(".source").css('background-image','url(/images/chris.jpg)');
+       //  }
+       //  else if (counter === 91){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 100){
+       //       $(".source").css('background-image','url(/images/chris.jpg)');
+       //  }
+       //  else if (counter === 101){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       // else if(counter === 130){
+       //       $(".source").css('background-image','url(/images/gw.jpg)');
+       //  }
+       //  else if (counter === 131){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       // else if(counter === 140){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+       //  }
+       //  else if (counter === 141){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       // else if(counter === 145){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+       //  }
+       //  else if (counter === 146){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       // else if(counter === 150){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+       //  }
+       //  else if (counter === 151){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 150){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+       //  }
+       //  else if (counter === 151){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 152){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+       //  }
+       //  else if (counter === 153){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 154){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+       //  }
+       //  else if (counter === 155){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if(counter === 156){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+       //  }
+       //  else if (counter === 157){
+       //       $(".source").css('background-image','url(/images/masterP.jpg)');
+             
+       //  }
+       //  else if (counter > 157 && counter < 167 ){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+             
+       //  }
+       //  else if (counter === 167){
+       //       $(".source").css('background-image','url(/images/juicyJ.jpg)');
+             
+       //  }
+       //  else if (counter ===  168){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+             
+       //  }
+       //  else if (counter === 177){
+       //       $(".source").css('background-image','url(/images/juicyJ.jpg)');
+             
+             
+       //  }
+       //  else if (counter === 178){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+             
+       //  }
+       //  else if (counter === 188){
+       //       $(".source").css('background-image','url(/images/juicyJ.jpg)');
+             
+       //  }
+       //  else if (counter === 189){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+             
+       //  }
+       //  else if (counter === 167){
+       //       $(".source").css('background-image','url(/images/juicyJ.jpg)');
+             
+       //  }
+       //  else if (counter === 168){
+       //       $(".source").css('background-image','url(/images/warhol.jpg)');
+             
+       //  }
+       //  else if (counter === 169 ){
+       //       $(".source").css('background-image','url(/images/ferrgy.jpg)');
+             
+       //  }
+       //  else if (counter > 170) {
+            
+       //       $(".source").removeClass(".source--hidden")
+       //       $(".source").css('background-image','url(/images/stop.png)');
+       //  }
+        else{
+            
+        }
+}
+
+function glitchSecondPic(){
+
+        if (SecondPicSeconds === 2) {
+              $(".source").css('background-image','url(/images/ferrgy.jpg)');
+
+        }
+        else if (SecondPicSeconds === 9){
+
+        }
+        else if (SecondPicSeconds === ahh){
+
+        }
+        else{
+
+        }
+}
+
+function clicker(){
+
+    $("#example1 .source").glitch(function(canvas){
+        $("#example1 .target").html(canvas);
+        effect: "fade"
+        
+    });
+
+    $(".source").addClass("source--hidden")
+
+ }
+
+
+function myStopFunction() {
+
+    clearInterval(i);
+ 
+}
+
+function myTimer(){
+    i++
+    if(i === 15){
+        $(".source").css('background-image','url(/images/play.png)');
+
+ 
+    }
+    else if( i === 17){
+        myStopFunction()
+        clicker()
+         
+    }
+    else if(i === 19){
+        console.log("let's goooooo")
+        counter = 0;
+        var a = setInterval(function(){
+      
+            $("button").trigger("click")
+            clicker()
+
+            counter++;
+
+            glitchPic()
+
+        }, 150); 
+
+
+        SecondPicSeconds = 0;
+        setInterval(function () {
+          ++SecondPicSeconds;
+
+          console.log(SecondPicSeconds)
+          if (SecondPicSeconds === 1 ) {
+             $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 2){
+             $(".source").css('background-image','url(/images/masterP.jpg)');
+          }
+          else if(SecondPicSeconds === 8 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 9 ){
+              $(".source").css('background-image','url(/images/masterP.jpg)');
+          }
+         else if(SecondPicSeconds === 12 ){
+              $(".source").css('background-image','url(/images/chris.jpg)');
+          }
+          else if(SecondPicSeconds === 15 - 1){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+              console.log("THIRD!!")
+          }
+          else if(SecondPicSeconds === 16 ){
+              $(".source").css('background-image','url(/images/chris.jpg)');
+          }
+          else if(SecondPicSeconds === 14 + 7 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 7){
+              $(".source").css('background-image','url(/images/chris.jpg)');
+          }   
+          else if(SecondPicSeconds === 14 + 14 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 14){
+              $(".source").css('background-image','url(/images/masterP.jpg)');
+          }  
+          else if(SecondPicSeconds === 14 + 21 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 21){
+              $(".source").css('background-image','url(/images/masterP.jpg)');
+          }  
+          else if(SecondPicSeconds === 14 + 28 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 28){
+              $(".source").css('background-image','url(/images/ferrgy.jpg)');
+          }   
+          else if(SecondPicSeconds === 14 + 35 - 1){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 35 - 1){
+              $(".source").css('background-image','url(/images/masterP.jpg)');
+          }   
+          else if(SecondPicSeconds === 14 + 42 - 1 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 42 - 1){
+              $(".source").css('background-image','url(/images/masterP.jpg)');
+          }    
+          else if(SecondPicSeconds === 14 + 49 - 1 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 49 - 1){
+              $(".source").css('background-image','url(/images/warhol.jpg)');
+          }  
+          else if(SecondPicSeconds === 14 + 56 - 1 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 56 - 1){
+              $(".source").css('background-image','url(/images/masterP.jpg)');
+          } 
+          else if(SecondPicSeconds === 14 + 63 - 2 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 63 - 2){
+              $(".source").css('background-image','url(/images/masterP.jpg)');
+          }   
+          else if(SecondPicSeconds === 14 + 71 - 3 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 71 - 3){
+              $(".source").css('background-image','url(/images/gw.jpg)');
+          } 
+          else if(SecondPicSeconds === 14 + 78 - 3 ){
+              $(".source").css('background-image','url(/images/Yr0DYmAB.jpg)');
+          }
+          else if(SecondPicSeconds === 15 + 78 - 3){
+              $(".source").css('background-image','url(/images/gw.jpg)');
+          }             
+          else{
+
+          }
+            ;
+        }, 1000);
+    }
+    else if(i >= 15){
+        
+         // $(".source--hidden").removeClass(".source--hidden")
+         // $(".source").css('background-image','url(/images/forward.png)');
+        
+    }
+    else{
+        
+         
+    }
+}
+
+
+var i = setInterval(function(){myTimer() }, 1000);
+
+
+
+
+  
+        
+
+
+});
---
+++ 08_echonestBeatPitch/p5CS.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    
+ //    function preload() {
+	//   mySound = loadSound('wanLovey.mp3');
+	// }
+
+	// function setup() {
+	//   mySound.setVolume(0.1);
+	//   mySound.play();
+	// }
+});
